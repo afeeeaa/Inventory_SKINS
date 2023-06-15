70de937c859440b8e84a4e6858d37c0773621118
anisamel <anisameilia233@gmail.com>
2023-06-15 20:11:08 +0700

    dashboard

---
+++ application/views/dashboard.php
@@ -0,0 +1,95 @@
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+    
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <!-- Small boxes (Stat box) -->
+      <?php if($is_admin == true): ?>
+
+        <div class="row">
+          <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-purple">
+              <div class="inner">
+                <h3><?php echo $total_brands ?></h3>
+
+                <h4><b>Total Brands</b></h4>
+              </div>
+              <div class="icon">
+                <i class="fa fa-cubes"></i>
+              </div>
+              <a href="<?php echo base_url('Controller_Items/') ?>" class="small-box-footer">More Info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-teal">
+              <div class="inner">
+                <h3><?php echo $total_category ?></h3>
+
+                <h4><b>Total Category</b></h4>
+              </div>
+              <div class="icon">
+                <i class="fa fa-th"></i>
+              </div>
+              <a href="<?php echo base_url('Controller_Category/') ?>" class="small-box-footer">More Info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-yellow">
+              <div class="inner">
+                <h3><?php echo $total_attribures; ?></h3>
+
+               <h4><b>Total Elements</h4></b>
+              </div>
+              <div class="icon">
+                <i class="fa fa-files-o"></i>
+              </div>
+              <a href="<?php echo base_url('Controller_Element/') ?>" class="small-box-footer">More Info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-primary">
+              <div class="inner">
+                <h3><?php echo $total_products ?></h3>
+
+               <h4><b>Total Products</b></h4>
+              </div>
+              <div class="icon">
+                <i class="fa fa-cube"></i>
+              </div>
+              <a href="<?php echo base_url('Controller_Products/') ?>" class="small-box-footer">More Info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+        </div>
+      <?php endif; ?>
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $("#dashboardMainMenu").addClass('active');
+    }); 
+  </script>
