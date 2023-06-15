b649a43811dba32703ab1771fa28a32258b3c838
anisamel <anisameilia233@gmail.com>
2023-06-15 20:14:40 +0700

    header menu

---
+++ application/views/templates/header_menu.php
@@ -0,0 +1,15 @@
+
+ 
+  <header class="main-header">
+    <!-- Logo -->
+    <a href="" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>I</b>CI</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"></i>INVENTORY SKINS</span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+    
+    </nav>
+  </header>
